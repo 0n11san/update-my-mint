d7b369d25d890559fc54db9817810a9732b0b9cc
0n11san <0n11san@users.noreply.github.com>
2018-06-02 10:20:31 -0400

    refactored for python v3

--- usage.py
+++ usage.py
@@ -1,3 +1,4 @@
+#updated some lines to work w/ Python v3 syntax (as opposed to v2)
 from Robinhood import Robinhood
 import mint
 
@@ -16,17 +17,19 @@ if __name__ == '__main__':
 
     my_accounts = {
         'Robinhood': {'callback': get_robinhood, 'login': 'login_here', 'password': 'password_here'},
-        'Coinspring': {'callback': get_coinspring, 'login': 'login_here', 'password': 'password_here'}
+        # I don't use Coinspring presently so commented out
+        # 'Coinspring': {'callback': get_coinspring, 'login': 'login_here', 'password': 'password_here'}
     }
 
     mint = mint.Mint(mint_login, mint_passwd)
 
     for account in mint.get_accounts():
-        if my_accounts.has_key(account['name']):
-            print 'found', account['name']
+        # if my_accounts.has_key(account['name']):
+        if account['name'] in my_accounts:
+            print ('found', account['name'])
             acc = my_accounts[account['name']]
             new_value = acc['callback'](acc['login'], acc['password'])
-            print 'updating', account['name'], 'with US$', new_value
+            print ('updating', account['name'], 'with US$', new_value)
             mint.set_property_account_value(account, new_value)
 
     mint.driver.close()
